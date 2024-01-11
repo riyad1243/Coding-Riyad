12de3b5f58350e7c2269a4a7f5a98b3400f20610
Riyad Sarkar <reyidkhan319@gmail.com>
2024-01-12 02:01:21 +0600

    new file created

---
+++ Chrome Extension/app.js
@@ -0,0 +1,15 @@
+// console.log("hello..")
+
+//TODO : Select all selectors
+//TODO : Make pick color btn functional
+//TODO : Make copy color functional
+//TODO : Show color on the DOM
+//TODO : Local Storage
+//TODO : Clear button to clear all the previous colors
+
+//Selectors
+const colorPickerBtn = document.getElementById("color-picker");
+const clearAll = document.querySelector("clear-all");
+const colorList = document.querySelector("all-colors");
+
+ 
